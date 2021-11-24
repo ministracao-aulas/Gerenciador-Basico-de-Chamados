2d3fed7353c0a38029d2eb259a0255a0deef2be9
Tiago França <tiago@tiagofranca.com>
2021-11-24 20:20:08 -0300

    Criado rota get para /logout e alterado redirecionamento
    No ramo 2-instalar-e-pacote-de-autenticacao
    Mudanças a serem submetidas:
    	modified:   app/Http/Controllers/Auth/LoginController.php
    	modified:   routes/web.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,17 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * function logout
+     *
+     * @param Request $request
+     * @return
+     */
+    public function logout(Request $request)
+    {
+        \Auth::logout();
+        return redirect()->route('login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/logout', [LoginController::class, 'logout'])->middleware('auth');
+
 Route::get('/', function() {
-    return redirect()->route('demo_index');
+    return redirect()->route('demo_dashboard');
 });
 Route::prefix('demo')->group(function () {
     App\Http\Controllers\Admin\DemoDashboardController::routes();
